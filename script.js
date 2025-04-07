f29f258d91fd265ffb0c4e620846a35b307e32fd
mr-jw <joshua.walley01@gmail.com>
2025-04-07 15:43:31 +0100

    refactor script

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
-function f1() {
-    var p = new Date();
-    var q = new Date("2025-12-25");
-  
-    var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
-  
-    var variable2 = document.getElementById("my-element");
-    variable2.innerText = variable;
-  }
-  
-  function f2() {
-    var p = new Date();
-    var q = new Date("2026-01-01");
-  
-    var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
-  
-    var variable2 = document.getElementById("my-element2");
-    variable2.innerText = variable;
-  }
-  
-  f1();
-  f2();
+
+function f3(targetDate, elementId) {
+  var p = new Date(); 
+  var q = new Date(targetDate); 
+
+  var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
+  var variable2 = document.getElementById(elementId);
+  variable2.innerText = variable;
+}
+
+f3("2025-12-25", "my-element");
+f3("2026-01-01", "my-element2");
   
