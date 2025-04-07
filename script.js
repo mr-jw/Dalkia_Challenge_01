1c0b4d727197ac569ab9b7286cb1cda1d72b6f1e
mr-jw <joshua.walley01@gmail.com>
2025-04-07 15:51:46 +0100

    add comments

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
+// Arguments: target date ("2025-12-25"), element to alter (my-element).
 function f3(targetDate, elementId) {
-  var p = new Date(); 
-  var q = new Date(targetDate); 
+  var p = new Date();           // current date.
+  var q = new Date(targetDate); // target date.
 
   var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
+
   var variable2 = document.getElementById(elementId);
   variable2.innerText = variable;
 }
